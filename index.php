39915473c923bfde106a48ce76b724c2ac4d0f18
Paul <paulrdkirby@gmail.com>
2016-03-04 01:03:41 -0700

    Set up commerce kickstart, created single page layout with all previous content, added products, added necessary contributed modules for site. Created Zen subtheme. Drupal 7 install. Should make note of contribs for future installs

--- index.php
+++ index.php
@@ -4,19 +4,30 @@
  * @file
  * The PHP page that serves all page requests on a Drupal installation.
  *
+ * The routines here dispatch control to the appropriate handler, which then
+ * prints the appropriate page.
+ *
  * All Drupal code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
+ * See COPYRIGHT.txt and LICENSE.txt.
  */
 
-use Drupal\Core\DrupalKernel;
-use Symfony\Component\HttpFoundation\Request;
+/**
+ * Root directory of Drupal installation.
+ */
+define('DRUPAL_ROOT', getcwd());
 
-$autoloader = require_once 'autoload.php';
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 
-$kernel = new DrupalKernel('prod', $autoloader);
-
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-
-$kernel->terminate($request, $response);
+try {
+  drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+}
+catch (PDOException $e) {
+  // A PDO exception might mean that we are running on an empty database. In
+  // that case, just redirect the user to install.php.
+  if (!db_table_exists('variable')) {
+    include_once DRUPAL_ROOT . '/includes/install.inc';
+    install_goto('install.php');
+  }
+  throw $e;
+}
+menu_execute_active_handler();
