dc0c0b7324dd9efebd297319ba8183ddd92e73fb
Paul <paulrdkirby@gmail.com>
2016-03-12 01:40:31 -0700

    made shop responsive

--- sites/all/themes/weathered_notebooks/js/frontpage.js
+++ sites/all/themes/weathered_notebooks/js/frontpage.js
@@ -15,19 +15,25 @@ jQuery(document).ready(function(){
 
 //* Dynamically set heights of products
 function shopResize() {
-	//Get the height of the two notebook objects
-    var height = jQuery("#shop .views-row-1").outerHeight(true);
 
-	// set the outer height of the two pocket books to be 3/5 height
-    jQuery("#shop .views-row-3, #shop .views-row-4, #shop .views-row-5")
-    .outerHeight(height * 0.6);
+	if (screen.width > 768) {
+		//Get the height of the two notebook objects
+	    var height = jQuery("#shop .views-row-1").outerHeight(true);
 
-	// set the outer height of the paper to be 2/5 height (+1 for some reason, cause math)
-    jQuery("#shop .views-row-5").outerHeight(height * 0.4 + 1);
-    var row5img = jQuery("#shop .views-row-5 .product-img");
-	// position the img at the bottom of the resulting div, to make perfect grid
-    row5img.css("position", "absolute");
-    row5img.css("bottom", "0");
+		// set the outer height of the two pocket books to be 3/5 height
+	    jQuery("#shop .views-row-3, #shop .views-row-4, #shop .views-row-5")
+	    .outerHeight(height * 0.6);
+
+		// set the outer height of the paper to be 2/5 height (+1 for some reason, cause math)
+	    jQuery("#shop .views-row-5").outerHeight(height * 0.4 + 1);
+	    var row5img = jQuery("#shop .views-row-5 .product-img");
+		// position the img at the bottom of the resulting div, to make perfect grid
+	    row5img.css("position", "absolute");
+	    row5img.css("bottom", "0");
+	} else {
+		var row5img = jQuery("#shop .views-row-5 .product-img");
+		row5img.css("position", "relative");
+	}
 };
 
 window.onload = function(){shopResize()};
